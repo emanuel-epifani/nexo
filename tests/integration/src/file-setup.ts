dd2ad49f80063406b1e0ab0ca190d718cbfce56a
emanuelepifani <emanuel.epifani.m@gmail.com>
2026-01-28 15:03:08 +0100

    [feat] add file setup

---
+++ tests/integration/src/file-setup.ts
@@ -0,0 +1,10 @@
+import { beforeAll, afterAll } from "vitest";
+
+beforeAll(async () => {
+    console.debug("→ beforeAll file");
+});
+
+
+afterAll(async () => {
+    console.debug("→ afterAll file");
+});
